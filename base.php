369c4ce42fb29c80add56a09a59bb767973671ef
vpnclashfa backup <213138203+vpnclashfa-backup@users.noreply.github.com>
2025-06-25 06:14:30 +0330

    Update base.php

--- base.php
+++ base.php
@@ -927,15 +927,15 @@ getTelegramChannelConfigs($source);
 
 $normals = addStringToBeginning(
     listFilesInDirectory("subscription/normal"),
-    "https://raw.githubusercontent.com/Shadow-130/HiN-VPN/main/"
+    "https://raw.githubusercontent.com/vpnclashfa-backup/HiN-VPN/main/"
 );
 $base64 = addStringToBeginning(
     listFilesInDirectory("subscription/base64"),
-    "https://raw.githubusercontent.com/Shadow-130/HiN-VPN/main/"
+    "https://raw.githubusercontent.com/vpnclashfa-backup/HiN-VPN/main/"
 );
 $hiddify = addStringToBeginning(
     listFilesInDirectory("subscription/hiddify"),
-    "https://raw.githubusercontent.com/Shadow-130/HiN-VPN/main/"
+    "https://raw.githubusercontent.com/vpnclashfa-backup/HiN-VPN/main/"
 );
 $protocolColumn = getFileNamesInDirectory(
     listFilesInDirectory("subscription/normal")
@@ -947,15 +947,15 @@ $cells1Array = convertArrays($protocolColumn, $normals, $base64, $hiddify);
 
 $sourceNormals = addStringToBeginning(
     listFilesInDirectory("subscription/source/normal"),
-    "https://raw.githubusercontent.com/Shadow-130/HiN-VPN/main/"
+    "https://raw.githubusercontent.com/vpnclashfa-backup/HiN-VPN/main/"
 );
 $sourceBase64 = addStringToBeginning(
     listFilesInDirectory("subscription/source/base64"),
-    "https://raw.githubusercontent.com/Shadow-130/HiN-VPN/main/"
+    "https://raw.githubusercontent.com/vpnclashfa-backup/HiN-VPN/main/"
 );
 $sourceHiddify = addStringToBeginning(
     listFilesInDirectory("subscription/source/hiddify"),
-    "https://raw.githubusercontent.com/Shadow-130/HiN-VPN/main/"
+    "https://raw.githubusercontent.com/vpnclashfa-backup/HiN-VPN/main/"
 );
 $sourcesColumn = getFileNamesInDirectory(
     listFilesInDirectory("subscription/source/normal")
@@ -1001,7 +1001,7 @@ $keyboard = [
         [
             "text" => "🚹 گیتهاب HiN VPN 🚹",
             "url" =>
-                "https://github.com/Shadow-130/HiN-VPN/blob/main/README.md",
+                "https://github.com/vpnclashfa-backup/HiN-VPN/blob/main/README.md",
         ],
     ],
 ];
